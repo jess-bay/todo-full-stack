b718dc94109f6b1195228e09fba339d76fa731de
Jess Bayman <justjessb@gmail.com>
2024-04-22 12:08:34 +1200

    Complete setup, re-install npm packages

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,13 +1,15 @@
+import AddTodo from './AddTodo'
+import { TodoList } from './TodoList'
 
 function App() {
   return (
     <>
       <header className="header">
         <h1>todos</h1>
-      
+        <AddTodo />
+        <TodoList />
       </header>
       <section className="main"></section>
-      <footer className="footer"></footer>
     </>
   )
 }
--- client/index.tsx
+++ client/index.tsx
@@ -1,16 +1,16 @@
-// import { createRoot } from 'react-dom/client'
-// import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-// import App from './components/App.tsx'
+import App from './components/App.tsx'
 
-// const queryClient = new QueryClient()
+const queryClient = new QueryClient()
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   createRoot(document.getElementById('app') as HTMLElement).render(
-//     <QueryClientProvider client={queryClient}>
-//       <App />
-//       <ReactQueryDevtools />
-//     </QueryClientProvider>
-//   )
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  createRoot(document.getElementById('app') as HTMLElement).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>,
+  )
+})
